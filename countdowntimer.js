764311087c88f0d4ffad56a2af054ec482424493
staffslide <dia.mizojiri@gmail.com>
2024-11-14 09:29:44 +0900

    Update countdowntimer.js

--- countdowntimer.js
+++ countdowntimer.js
@@ -9,18 +9,18 @@ function countDown(unixEndDate) {
   // 目標時刻と現時刻の差をUnixTimeで計算
   let leftDate = unixEndDate - unixCurrentDate;
   // 目標時刻と現時刻の差から残り時間を計算
-  let leftDayDate = Math.floor(leftDate / (1000 * 60 * 60 * 24));
+  //let leftDayDate = Math.floor(leftDate / (1000 * 60 * 60 * 24));
   let leftHourDate = Math.floor((leftDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) + ((leftDate / (1000 * 60 * 60 * 24))) * 24);
   let leftMinuteDate = Math.floor((leftDate % (1000 * 60 * 60)) / (1000 * 60));
   let leftSecondDate = Math.floor((leftDate % (1000 * 60)) / 1000);
   // 残り時間を表示させる
   if (leftDate < 0){
-    day.innerHTML = "0";
+    //day.innerHTML = "0";
     hour.innerHTML = "0";
     minute.innerHTML = "0";
     second.innerHTML = "0";
   } else {
-    day.innerHTML = `${leftDayDate}`;
+   // day.innerHTML = `${leftDayDate}`;
     hour.innerHTML = `${leftHourDate}`;
     minute.innerHTML = `${leftMinuteDate}`;
     second.innerHTML = `${leftSecondDate}`;
